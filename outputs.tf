3d65b4c67a587171f40d1a8ebc7fab2ba59550ef
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:23 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "cdn_frontdoor_security_policies" {
-  description = "All cdn_frontdoor_security_policy resources"
-  value       = azurerm_cdn_frontdoor_security_policy.cdn_frontdoor_security_policies
-}
 output "cdn_frontdoor_security_policies_cdn_frontdoor_profile_id" {
-  description = "List of cdn_frontdoor_profile_id values across all cdn_frontdoor_security_policies"
-  value       = [for k, v in azurerm_cdn_frontdoor_security_policy.cdn_frontdoor_security_policies : v.cdn_frontdoor_profile_id]
+  description = "Map of cdn_frontdoor_profile_id values across all cdn_frontdoor_security_policies, keyed the same as var.cdn_frontdoor_security_policies"
+  value       = { for k, v in azurerm_cdn_frontdoor_security_policy.cdn_frontdoor_security_policies : k => v.cdn_frontdoor_profile_id }
 }
 output "cdn_frontdoor_security_policies_name" {
-  description = "List of name values across all cdn_frontdoor_security_policies"
-  value       = [for k, v in azurerm_cdn_frontdoor_security_policy.cdn_frontdoor_security_policies : v.name]
+  description = "Map of name values across all cdn_frontdoor_security_policies, keyed the same as var.cdn_frontdoor_security_policies"
+  value       = { for k, v in azurerm_cdn_frontdoor_security_policy.cdn_frontdoor_security_policies : k => v.name }
 }
 output "cdn_frontdoor_security_policies_security_policies" {
-  description = "List of security_policies values across all cdn_frontdoor_security_policies"
-  value       = [for k, v in azurerm_cdn_frontdoor_security_policy.cdn_frontdoor_security_policies : v.security_policies]
+  description = "Map of security_policies values across all cdn_frontdoor_security_policies, keyed the same as var.cdn_frontdoor_security_policies"
+  value       = { for k, v in azurerm_cdn_frontdoor_security_policy.cdn_frontdoor_security_policies : k => v.security_policies }
 }
 
